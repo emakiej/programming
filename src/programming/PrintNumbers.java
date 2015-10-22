706b752fc5f018d04aa70e4763ea5234f224acf9
Malgorzata Chmielewska <mkudzia@opera.com>
2015-10-22 17:03:40 +0200

    comments added

--- src/programming/PrintNumbers.java
+++ src/programming/PrintNumbers.java
@@ -1,17 +1,26 @@
 package programming;
 
+/**
+ * Prints prime numbers from 2 to X
+ * @author gosia
+ *
+ */
 public class PrintNumbers {
 	private static int X = 0;
 
 	public static void main(String[] args) {
 		if (checkInputData(args)) {
-			for (int i = 2; i < X; i++) {
-				if (!isNotPrime(i))
-					System.out.println(i);
+			for (int number = 2; number < X; number++) {
+				if (!isNotPrime(number)) System.out.println(number);
 			}
 		}
 	}
 
+	/**
+	 * Checks if input data are correct and greater than 2
+	 * @param numbers - input data
+	 * @return True if input data are correct or greater than 2, otherwise false
+	 */
 	private static boolean checkInputData(String[] numbers) {
 		if (numbers.length > 0) {
 			try {
@@ -19,7 +28,7 @@ public class PrintNumbers {
 				if (X >= 2) {
 					return true;
 				} else {
-					System.out.println("The number should be greather than 2");
+					System.out.println("The number should be greater than 2");
 					return false;
 				}
 			} catch (NumberFormatException e) {
@@ -32,9 +41,14 @@ public class PrintNumbers {
 		}
 	}
 
-	private static boolean isNotPrime(int i) {
-		for (int j = 2; j < i; j++) {
-			if (i % j == 0) {
+	/**
+	 * Checks if number is not prime
+	 * @param number to check if is not prime
+	 * @return true if is not prime, otherwise false
+	 */
+	private static boolean isNotPrime(int number) {
+		for (int j = 2; j < number; j++) {
+			if (number % j == 0) {
 				return true;
 			}
 		}
