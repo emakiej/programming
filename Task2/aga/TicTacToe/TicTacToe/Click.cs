d8cf518a6e6d65d09bfd6b559a5ecc6163057cef
koonik <a.malarska@gmail.com>
2016-07-10 16:44:05 +0200

    TicTacToe v1
    TicTacToe game

---
+++ Task2/aga/TicTacToe/TicTacToe/Click.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    internal class Click
+    {
+        private int turn = 1;
+        public Click()
+            { }
+
+        public void TurnCounter()
+            {
+                turn++;
+            }
+        public int GetTurn()
+        {
+            return turn;
+        }
+    }
+}
